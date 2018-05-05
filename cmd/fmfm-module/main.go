1757b26598b4bd2d594aca89be9ba0e2ecc1c712
but80 <mersenne.sister@gmail.com>
2018-05-05 23:53:00 +0900

    C-shared module

--- cmd/fmfm-module/main.go
+++ cmd/fmfm-module/main.go
@@ -2,12 +2,10 @@ package main
 
 import (
 	"C"
-
-	"github.com/but80/fmfm/ymf"
-)
-import (
 	"time"
 
+	"github.com/but80/fmfm"
+	"github.com/but80/fmfm/ymf"
 	"github.com/but80/smaf825/smaf/voice"
 )
 
@@ -17,14 +15,15 @@ func main() {
 
 var chip *ymf.Chip
 
-func Init(string voicePath) {
+//export Init
+func Init(sampleRate float64, voicePath string) {
 	lib, err := voice.NewVM5VoiceLib(voicePath)
 	if err != nil {
 		panic(err)
 	}
 
-	chip := ymf.NewChip(renderer.Parameters.SampleRate)
-	seq.Reset()
-	renderer.Start(chip.Next)
+	chip := ymf.NewChip(sampleRate)
+	ctrl := fmfm.NewController(chip, lib)
+	ctrl.Reset()
 	time.Sleep(24 * time.Hour)
 }
