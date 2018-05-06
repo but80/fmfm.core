619a5938e2d10945f4756f0e66eb3b869aeeffab
but80 <mersenne.sister@gmail.com>
2018-05-06 21:57:29 +0900

    Implement c-shared module

--- cmd/fmfm-module/main.go
+++ cmd/fmfm-module/main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"C"
-	"time"
+	"sync"
 
 	"github.com/but80/fmfm"
 	"github.com/but80/fmfm/ymf"
@@ -13,17 +13,62 @@ func main() {
 	// noop
 }
 
+var lib *voice.VM5VoiceLib
 var chip *ymf.Chip
+var ctrl *fmfm.Controller
+var initOnce sync.Once
 
-//export Init
-func Init(sampleRate float64, voicePath string) {
-	lib, err := voice.NewVM5VoiceLib(voicePath)
-	if err != nil {
-		panic(err)
-	}
+// FMFMInit は、音源を初期化します。
+//export FMFMInit
+func FMFMInit(sampleRate C.double, voicePath *C.char) C.int {
+	result := 0
+	initOnce.Do(func() {
+		var err error
+		lib, err = voice.NewVM5VoiceLib(C.GoString(voicePath))
+		if err != nil {
+			panic(err)
+		}
+		chip = ymf.NewChip(float64(sampleRate))
+		ctrl = fmfm.NewController(chip, lib)
+		ctrl.Reset()
+		result = 1
+	})
+	return C.int(result)
+}
 
-	chip := ymf.NewChip(sampleRate)
-	ctrl := fmfm.NewController(chip, lib)
-	ctrl.Reset()
-	time.Sleep(24 * time.Hour)
+// FMFMNoteOn は、MIDIノートオン受信時の音源の振る舞いを再現します。
+//export FMFMNoteOn
+func FMFMNoteOn(ch, note, velocity C.longlong) {
+	ctrl.NoteOn(int(ch), int(note), int(velocity))
+}
+
+// FMFMNoteOff は、MIDIノートオフ受信時の音源の振る舞いを再現します。
+//export FMFMNoteOff
+func FMFMNoteOff(ch, note C.longlong) {
+	ctrl.NoteOff(int(ch), int(note))
+}
+
+// FMFMControlChange は、MIDIコントロールチェンジ受信時の音源の振る舞いを再現します。
+//export FMFMControlChange
+func FMFMControlChange(ch, cc, value C.longlong) {
+	ctrl.ControlChange(int(ch), int(cc), int(value))
+}
+
+// FMFMProgramChange は、MIDIプログラムチェンジ受信時の音源の振る舞いを再現します。
+//export FMFMProgramChange
+func FMFMProgramChange(ch, value C.longlong) {
+	ctrl.ProgramChange(int(ch), int(value))
+}
+
+// FMFMPitchBend は、MIDIピッチベンド受信時の音源の振る舞いを再現します。
+//export FMFMPitchBend
+func FMFMPitchBend(ch, l, h C.longlong) {
+	ctrl.PitchBend(int(ch), int(l), int(h))
+}
+
+// FMFMNext は、次のサンプルを生成・取得します。
+//export FMFMNext
+func FMFMNext() (C.double, C.double) {
+	l, r := chip.Next()
+	return C.double(l), C.double(r)
 }
