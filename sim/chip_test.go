62640bcd3034032b87755e177f26e34ab48c3c33
but80 <mersenne.sister@gmail.com>
2018-05-18 22:50:02 +0900

    Fix fuzz testing of voice library data

--- sim/chip_test.go
+++ sim/chip_test.go
@@ -11,41 +11,47 @@ import (
 
 func TestNewChip(t *testing.T) {
 	sampleRate := 44100.0
+	f := fuzz.New()
 
 	for i := 0; i < 1000; i++ {
-		var lib smaf.VM5VoiceLib
-		fuzz.New().Fuzz(&lib)
-		lib.Normalize()
-		func() {
-			defer func() {
-				err := recover()
-				if err != nil {
-					t.Fatal(err)
-				}
-			}()
+		pc := smaf.VM35VoicePC{}
+		f.Fuzz(&pc)
+		pc.BankMsb = 0
+		pc.BankLsb = 0
+		pc.Pc = 0
+		pc.DrumNote = 0
+		pc.VoiceType = smaf.VoiceType_FM
+		pc.FmVoice = &smaf.VM35FMVoice{}
+		f.Fuzz(&pc.FmVoice)
 
+		lib := &smaf.VM5VoiceLib{Programs: []*smaf.VM35VoicePC{&pc}}
+		lib.Normalize()
+
+		func() {
 			chip := sim.NewChip(sampleRate, -15.0, -1)
 			regs := sim.NewRegisters(chip)
 			opts := &fmfm.ControllerOpts{
 				Registers: regs,
-				Libraries: []*smaf.VM5VoiceLib{&lib},
+				Libraries: []*smaf.VM5VoiceLib{lib},
 			}
 			seq := fmfm.NewController(opts)
 			chip.Next()
 
-			seq.PushMIDIMessage(fmfm.MIDIProgramChange, 0, 0, 0, 0)
-			seq.FlushMIDIMessages(0)
+			seq.PushMIDIMessage(fmfm.MIDIControlChange, 1, 0, 0, 0)
+			seq.PushMIDIMessage(fmfm.MIDIControlChange, 1, 0, 32, 0)
+			seq.PushMIDIMessage(fmfm.MIDIProgramChange, 1, 0, 0, 0)
+			seq.FlushMIDIMessages(2)
 			chip.Next()
 
-			seq.PushMIDIMessage(fmfm.MIDINoteOn, 0, 0, 60, 0)
-			seq.FlushMIDIMessages(0)
-			for j := 0; j < 1000; j++ {
+			seq.PushMIDIMessage(fmfm.MIDINoteOn, 3, 0, 60, 127)
+			seq.FlushMIDIMessages(4)
+			for j := 0; j < 100; j++ {
 				chip.Next()
 			}
 
-			seq.PushMIDIMessage(fmfm.MIDINoteOff, 0, 0, 60, 0)
-			seq.FlushMIDIMessages(0)
-			for j := 0; j < 1000; j++ {
+			seq.PushMIDIMessage(fmfm.MIDINoteOff, 5, 0, 60, 0)
+			seq.FlushMIDIMessages(6)
+			for j := 0; j < 100; j++ {
 				chip.Next()
 			}
 		}()
